7cc126880b52d81d16498dfde9b236aad7168829
Abhinesh Garhwal <abhinesh.garhwal@gmail.com>
2019-02-07 08:28:36 +0530

    Create Exercise_3.py

---
+++ Importing Data in Python (Part 2)/Exercise_3.py
@@ -0,0 +1,119 @@
+# EX-3
+
+#1
+# Import package
+import tweepy
+
+# Store OAuth authentication credentials in relevant variables
+access_token = "AAA"
+access_token_secret = "BBB"
+consumer_key = "CCC"
+consumer_secret = "DDD"
+
+# Pass OAuth details to tweepy's OAuth handler
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+
+#2
+# Stream listener class
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api=None):
+        super(MyStreamListener, self).__init__()
+        self.num_tweets = 0
+        self.file = open("tweets.txt", "w")
+
+    def on_status(self, status):
+        tweet = status._json
+        self.file.write( json.dumps(tweet) + '\n' )
+        self.num_tweets += 1
+        if self.num_tweets < 100:
+            return True
+        else:
+            return False
+        self.file.close()
+
+    def on_error(self, status):
+        print(status)
+
+
+# Initialize Stream listener
+l = MyStreamListener()
+
+# Create your Stream object with authentication
+stream = tweepy.Stream(auth, l)
+
+
+# Filter Twitter Streams to capture data by the keywords:
+stream.filter(track=['clinton', 'trump', 'sanders', 'cruz'])
+
+
+
+#3
+# Import package
+import json
+
+# String of path to file: tweets_data_path
+tweets_data_path = 'tweets.txt'
+
+# Initialize empty list to store tweets: tweets_data
+tweets_data = []
+
+# Open connection to file
+tweets_file = open(tweets_data_path, "r")
+
+# Read in tweets and store in list: tweets_data
+for line in tweets_file:
+    tweet = json.loads(line)
+    tweets_data.append(tweet)
+
+# Close connection to file
+tweets_file.close()
+
+# Print the keys of the first tweet dict
+print(tweets_data[0].keys())
+
+
+
+#4
+# Import package
+import pandas as pd
+
+# Build DataFrame of tweet texts and languages
+df = pd.DataFrame(tweets_data, columns=['text', 'lang'])
+
+# Print head of DataFrame
+print(df.head())
+
+
+
+#5
+# Initialize list to store tweet counts
+[clinton, trump, sanders, cruz] = [0, 0, 0, 0]
+
+# Iterate through df, counting the number of tweets in which
+# each candidate is mentioned
+for index, row in df.iterrows():
+    clinton += word_in_text('clinton', row['text'])
+    trump += word_in_text('trump', row['text'])
+    sanders += word_in_text('sanders', row['text'])
+    cruz += word_in_text('cruz', row['text'])
+
+
+
+#6
+# Import packages
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Set seaborn style
+sns.set(color_codes=True)
+
+# Create a list of labels:cd
+cd = ['clinton', 'trump', 'sanders', 'cruz']
+
+# Plot histogram
+ax = sns.barplot(cd, [clinton, trump, sanders, cruz])
+ax.set(ylabel="count")
+plt.show()
