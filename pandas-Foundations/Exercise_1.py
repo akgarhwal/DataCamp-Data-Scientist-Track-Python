c5204ee711ac740169fa1341113a963da1defff8
Abhinesh Garhwal <abhinesh.garhwal@gmail.com>
2019-02-11 07:54:20 +0530

    Update Exercise_1.py

--- pandas-Foundations/Exercise_1.py
+++ pandas-Foundations/Exercise_1.py
@@ -89,3 +89,40 @@ df2.to_csv(file_clean, index=False)
 # Save the cleaned up DataFrame to an excel file without the index
 df2.to_excel('file_clean.xlsx', index=False)
 
+
+#7
+# Create a plot with color='red'
+df.plot(color='red')
+
+# Add a title
+plt.title('Temperature in Austin')
+
+# Specify the x-axis label
+plt.xlabel('Hours since midnight August 1, 2010')
+
+# Specify the y-axis label
+plt.ylabel('Temperature (degrees F)')
+
+# Display the plot
+plt.show()
+
+
+
+#8
+# Plot all columns (default)
+df.plot()
+plt.show()
+
+# Plot all columns as subplots
+df.plot(subplots=True)
+plt.show()
+
+# Plot just the Dew Point data
+column_list1 = ['Dew Point (deg F)']
+df[column_list1].plot()
+plt.show()
+
+# Plot the Dew Point and Temperature data, but not the Pressure data
+column_list2 = ['Temperature (deg F)','Dew Point (deg F)']
+df[column_list2].plot()
+plt.show()
