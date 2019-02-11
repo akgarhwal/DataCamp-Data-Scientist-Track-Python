6fc3251b67846ea03e4b1201f86a8231a15514ff
Abhinesh Garhwal <abhinesh.garhwal@gmail.com>
2019-02-11 07:47:18 +0530

    Update Exercise_1.py

--- pandas-Foundations/Exercise_1.py
+++ pandas-Foundations/Exercise_1.py
@@ -15,3 +15,77 @@ df_log10 = np.log10(df)
 
 # Print original and new data containers
 [print(x, 'has type', type(eval(x))) for x in ['np_vals', 'np_vals_log10', 'df', 'df_log10']]
+
+
+
+#2
+# Zip the 2 lists together into one list of (key,value) tuples: zipped
+zipped = list(zip(list_keys, list_values))
+
+# Inspect the list using print()
+print(zipped)
+
+# Build a dictionary with the zipped list: data
+data = dict(zipped)
+
+# Build and inspect a DataFrame from the dictionary: df
+df = pd.DataFrame(data)
+print(df)
+
+
+#3
+# Build a list of labels: list_labels
+list_labels = ['year', 'artist', 'song', 'chart weeks']
+
+# Assign the list of labels to the columns attribute: df.columns
+df.columns = list_labels
+
+
+#4
+# Make a string with the value 'PA': state
+state = 'PA'
+
+# Construct a dictionary: data
+data = {'state': state, 'city': cities}
+
+# Construct a DataFrame from dictionary data: df
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print(df)
+
+
+#5
+# Read in the file: df1
+df1 = pd.read_csv(data_file)
+
+# Create a list of the new column labels: new_labels
+new_labels = ['year', 'population']
+
+# Read in the file, specifying the header and names parameters: df2
+df2 = pd.read_csv(data_file, header=0, names=new_labels)
+
+# Print both the DataFrames
+print(df1)
+print(df2)
+
+
+#6
+# Read the raw file as-is: df1
+df1 = pd.read_csv(file_messy)
+
+# Print the output of df1.head()
+print(df1.head())
+
+# Read in the file with the correct parameters: df2
+df2 = pd.read_csv(file_messy, delimiter=' ', header=3, comment='#')
+
+# Print the output of df2.head()
+print(df2.head())
+
+# Save the cleaned up DataFrame to a CSV file without the index
+df2.to_csv(file_clean, index=False)
+
+# Save the cleaned up DataFrame to an excel file without the index
+df2.to_excel('file_clean.xlsx', index=False)
+
