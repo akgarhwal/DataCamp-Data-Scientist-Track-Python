a5dbd2d4d077d21f341c71449a15abdc7c976160
Abhinesh Garhwal <abhinesh.garhwal@gmail.com>
2019-03-17 09:57:29 +0530

    Update Exercise_1.py

--- Introduction to Databases in Python/Exercise_1.py
+++ Introduction to Databases in Python/Exercise_1.py
@@ -1,1 +1,79 @@
 # EX-1
+#1
+# Import create_engine
+from sqlalchemy import create_engine
+
+# Create an engine that connects to the census.sqlite file: engine
+engine = create_engine("sqlite:///census.sqlite")
+
+# Print table names
+print(engine.table_names())
+
+
+#2
+# Import Table
+from sqlalchemy import Table
+
+# Reflect census table from the engine: census
+census = Table('census', metadata, autoload=True, autoload_with=engine)
+
+# Print census table metadata
+print(repr(census))
+
+
+
+#3
+# Reflect the census table from the engine: census
+census = Table('census', metadata, autoload=True, autoload_with=engine)
+
+# Print the column names
+print(census.columns.keys())
+
+# Print full table metadata
+print(repr(metadata.tables['census']))
+
+
+
+#4
+# Build select statement for census table: stmt
+stmt = 'SELECT * FROM census'
+
+# Execute the statement and fetch the results: results
+results = connection.execute(stmt).fetchall()
+
+# Print results
+print(results)
+
+
+#5
+# Import select
+from sqlalchemy import select
+
+# Reflect census table via engine: census
+census = Table('census', metadata, autoload=True, autoload_with=engine)
+
+# Build select statement for census table: stmt
+stmt = select([census])
+
+# Print the emitted statement to see the SQL emitted
+print(stmt)
+
+# Execute the statement and print the results
+print(connection.execute(stmt).fetchall())
+
+
+
+#6
+# Get the first row of the results by using an index: first_row
+first_row = results[0]
+
+# Print the first row of the results
+print(first_row)
+
+# Print the first column of the first row by using an index
+print(first_row[0])
+
+# Print the 'state' column of the first row by using its name
+print(first_row['state'])
+
+# THE END
